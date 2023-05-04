52f83b349ea52dbe813a456941ca0ba7145c3a76
btb0 <benburke2016@gmail.com>
2023-05-04 01:56:00 -0400

    adds fake db

---
+++ models/skill.js
@@ -0,0 +1,8 @@
+const skills = [
+    {id: 041121, skill: 'JavaScript'},
+    {id: 920207, skill: 'HTML & CSS'},
+    {id: 210411, skill: 'Express.js'},
+    {id: 210411, skill: 'jQuery'},
+    {id: 210411, skill: 'RESTful Routing'},
+    {id: 210411, skill: 'EJS'}
+  ];
