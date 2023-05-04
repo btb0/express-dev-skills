ae456df3864c2b16195c53a26612074bc56d9e88
btb0 <benburke2016@gmail.com>
2023-05-04 16:49:55 -0400

    updates fake db

--- models/skill.js
+++ models/skill.js
@@ -1,10 +1,12 @@
 const skills = [
-    {id: 041121, skill: 'JavaScript'},
-    {id: 920207, skill: 'HTML & CSS'},
-    {id: 210411, skill: 'Express.js'},
-    {id: 321031, skill: 'jQuery'},
-    {id: 530510, skill: 'RESTful Routing'},
-    {id: 621309, skill: 'EJS'}
+    {id: 041121, skill: 'JavaScript', learning: 'yes'},
+    {id: 920207, skill: 'HTML & CSS', learning: 'yes'},
+    {id: 210411, skill: 'Express.js', learning: 'yes'},
+    {id: 321031, skill: 'jQuery', learning: 'yes'},
+    {id: 530510, skill: 'RESTful Routing', learning: 'yes'},
+    {id: 621309, skill: 'EJS', learning: 'yes'},
+    {id: 120744, skill: 'React', learning: 'not yet'},
+    {id: 912301, skill: 'Python', learning: 'not yet'}
   ];
 
   module.exports = {
