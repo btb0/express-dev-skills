2ae12cfff3730b8f68b438903a8fcd29bc75c7a4
btb0 <benburke2016@gmail.com>
2023-05-04 17:55:23 -0400

    adds show functionality

--- models/skill.js
+++ models/skill.js
@@ -1,18 +1,26 @@
 const skills = [
-    {id: 041121, skill: 'JavaScript', learning: 'yes'},
-    {id: 920207, skill: 'HTML & CSS', learning: 'yes'},
-    {id: 210411, skill: 'Express.js', learning: 'yes'},
-    {id: 321031, skill: 'jQuery', learning: 'yes'},
-    {id: 530510, skill: 'RESTful Routing', learning: 'yes'},
-    {id: 621309, skill: 'EJS', learning: 'yes'},
-    {id: 120744, skill: 'React', learning: 'not yet'},
-    {id: 912301, skill: 'Python', learning: 'not yet'}
+    {id: 041121, skill: 'JavaScript', learning: true},
+    {id: 920207, skill: 'HTML & CSS', learning: true},
+    {id: 210411, skill: 'Express.js', learning: true},
+    {id: 321031, skill: 'jQuery', learning: true},
+    {id: 530510, skill: 'RESTful Routing', learning: true},
+    {id: 621309, skill: 'EJS', learning: true},
+    {id: 120744, skill: 'React', learning: false},
+    {id: 912301, skill: 'Python', learning: false}
   ];
 
   module.exports = {
-    getAll
+    getAll,
+    getOne
   }
 
+  // Returns all skills
   function getAll() {
     return skills
+  }
+
+  // Returns one skill
+  function getOne(id) {
+    id = parseInt(id)
+    return skills.find(skill => skill.id === id)
   }
